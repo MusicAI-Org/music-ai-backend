292ae5e0a356b8227e32c2283f1176833fe1e0e7
PAARTH2608 <jain.paarth2608@gmail.com>
2023-04-09 01:44:15 +0530

    add:node-cache&scheduler

--- api/controllers/User/Music/basicMusicController.js
+++ api/controllers/User/Music/basicMusicController.js
@@ -7,6 +7,10 @@
 // this route will be called automatically on the front page so that user has to login to spotify first then can see data
 const { google } = require("googleapis");
 const AuthenticatedUserModel = require("../../../models/User/Auth/AuthenticatedUser.model");
+const YoutubeDataModel = require("../../../models/YoutubeData/YoutubeData.model");
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
+const schedule = require("node-schedule");
 require("dotenv").config();
 
 const youtube = google.youtube({
@@ -17,58 +21,117 @@ const youtube = google.youtube({
 const getBasicMusicData = async (req, res) => {
   const currentYear = new Date().getFullYear();
   try {
-    // Fetch most popular music videos
-    const mostPopular = youtube.videos.list({
-      part: "snippet,statistics",
-      chart: "mostPopular",
-      q:`Most Popular Music`,
-      maxResults: 20,
-    });
-    // Fetch top tracks
-    const topTracks = youtube.search.list({
-      part: "snippet",
-      type: "video",
-      q: `Top tracks ${currentYear}`,
-      maxResults: 20,
-    });
-    // Fetch top artists
-    const topArtists = youtube.search.list({
-      part: "snippet",
-      type: "channel",
-      q: `Top artists ${currentYear}`,
-      maxResults: 20,
-    });
+    // Check if data exists in the cache
+    const cacheKey = "youtubeData";
+    const cachedData = cache.get(cacheKey);
+    if (cachedData) {
+      // If data exists in the cache, return it as response
+      console.log("Data retrieved from cache");
+      res.status(200).json(cachedData);
+    } else {
+      // If data does not exist in the cache, fetch data from YouTube API
+      console.log("Data not found in cache");
+      const mostPopular = youtube.videos.list({
+        part: "snippet,statistics",
+        chart: "mostPopular",
+        q: `Most Popular Music`,
+        maxResults: 20,
+      });
+      const topTracks = youtube.search.list({
+        part: "snippet",
+        type: "video",
+        q: `Top tracks ${currentYear}`,
+        maxResults: 20,
+      });
+      const topArtists = youtube.search.list({
+        part: "snippet",
+        type: "channel",
+        q: `Top artists ${currentYear}`,
+        maxResults: 20,
+      });
+      const topAlbums = youtube.search.list({
+        part: "snippet",
+        type: "album",
+        q: `Top albums ${currentYear}`,
+        maxResults: 20,
+      });
 
-    // Fetch top albums
-    const topAlbums = youtube.search.list({
-      part: "snippet",
-      type: "album",
-      q: `Top albums ${currentYear}`,
-      maxResults: 20,
-    });
+      const combinedData = await Promise.all([
+        mostPopular,
+        topTracks,
+        topArtists,
+        topAlbums,
+      ]);
+      const mostPopularData = combinedData[0].data.items;
+      const topTracksData = combinedData[1].data.items;
+      const topArtistsData = combinedData[2].data.items;
+      const topAlbumsData = combinedData[3].data.items;
 
-    const combinedData = await Promise.all([
-      mostPopular,
-      topTracks,
-      topArtists,
-      topAlbums,
-    ]);
-    const mostPopularData = combinedData[0].data.items;
-    const topTracksData = combinedData[1].data.items;
-    const topArtistsData = combinedData[2].data.items;
-    const topAlbumsData = combinedData[3].data.items;
+      // Store fetched data in cache with a time-to-live (TTL) of 24 hours
+      const newData = {
+        mostPopularData,
+        topTracksData,
+        topArtistsData,
+        topAlbumsData,
+      };
+      cache.set(cacheKey, newData, 24 * 60 * 60);
 
-    const data = {
-      mostPopularData,
-      topTracksData,
-      topArtistsData,
-      topAlbumsData,
-    };
+      // Create a scheduled task to renew the data in the cache after 24 hours
+      schedule.scheduleJob("0 0 * * *", async () => {
+        console.log("Data in cache expired, renewing...");
+        const mostPopular = youtube.videos.list({
+          part: "snippet,statistics",
+          chart: "mostPopular",
+          q: `Most Popular Music`,
+          maxResults: 20,
+        });
+        const topTracks = youtube.search.list({
+          part: "snippet",
+          type: "video",
+          q: `Top tracks ${currentYear}`,
+          maxResults: 20,
+        });
+        const topArtists = youtube.search.list({
+          part: "snippet",
+          type: "channel",
+          q: `Top artists ${currentYear}`,
+          maxResults: 20,
+        });
+        const topAlbums = youtube.search.list({
+          part: "snippet",
+          type: "album",
+          q: `Top albums ${currentYear}`,
+          maxResults: 20,
+        });
 
-    res.status(200).json(data);
+        const renewedData = await Promise.all([
+          mostPopular,
+          topTracks,
+          topArtists,
+          topAlbums,
+        ]);
+        const renewedMostPopularData = renewedData[0].data.items;
+        const renewedTopTracksData = renewedData[1].data.items;
+        const renewedTopArtistsData = renewedData[2].data.items;
+        const renewedTopAlbumsData = renewedData[3].data.items;
+
+        // Update the data in the cache with renewed data
+        const updatedData = {
+          mostPopularData: renewedMostPopularData,
+          topTracksData: renewedTopTracksData,
+          topArtistsData: renewedTopArtistsData,
+          topAlbumsData: renewedTopAlbumsData,
+        };
+        cache.set(cacheKey, updatedData, 24 * 60 * 60);
+        console.log("Data in cache renewed");
+      });
+
+      // Return the fetched data as response
+      res.status(200).json(newData);
+    }
   } catch (error) {
-    console.error("Error fetching data from YouTube Music API", error);
-    res.status(500).json({ error: "Unable to fetch music data" });
+    console.error("Failed to fetch data from YouTube API", error);
+    res.status(500).json({ message: "Failed to fetch data from YouTube API" });
   }
 };
 
